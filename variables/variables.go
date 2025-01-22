7745907c1dfc8e1a6191384a2f3c8f1bc422d575
Ritam <ritamdutta87778@gmail.com>
2025-01-22 22:46:55 +0530

    Variables in Golang

---
+++ variables/variables.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//strings
+
+	var name string = "John Doe"
+	
+
+	fmt.Println(name)
+
+	//integers
+
+	var age int = 42
+
+	fmt.Println(age)
+
+	//floats
+
+	var temperature float64 = 20.5
+
+	fmt.Println(temperature)
+
+	//booleans
+
+	var isRaining bool = true
+
+	fmt.Println(isRaining)
+
+	// Shorthand syntax
+
+	 name2 := "Ritam"
+
+	 fmt.Println(name2)
+
+	 var name3 string
+
+	 name3 = "Ricky"
+
+	 fmt.Println(name3)
+}
