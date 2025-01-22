5b92cb4add0eccfc06513c616c7521090c2b839e
Ritam <ritamdutta87778@gmail.com>
2025-01-22 21:17:30 +0530

    simple_values

---
+++ 02simple_values/main.go
@@ -0,0 +1,20 @@
+// Data types
+
+package main
+
+import "fmt"
+
+func main() {
+	// Strings
+	fmt.Println("Hello, World!")
+
+	// Integers
+	fmt.Println(42)
+
+	// Floats
+	fmt.Println(3.14159)
+
+	// Booleans
+	fmt.Println(true)
+	fmt.Println(false)
+}
